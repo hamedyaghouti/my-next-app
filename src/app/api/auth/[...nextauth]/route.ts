8800549a1c8d8de69c64807d2909ed5123117820
test <test@gmail.com>
2023-10-02 17:54:06 +0330

    1111111

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,14 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/app/libs/auth-options";
 
-const runtime = "edge";
+// export const runtime = "edge";
 
 // @ts-ignore
 const handler = NextAuth(authOptions);
 
-export { runtime, handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
+
+export const GET = handler.handlers.GET;
+export const POST = handler.handlers.POST;
+
+export const runtime = "edge";
