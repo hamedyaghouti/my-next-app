de5117973bc7a6fd6c7d37dcc97808429f378dfc
h.yaghooti <hamedyaghooti1992@gmail.com>
2023-10-06 18:16:45 +0330

    test-debug

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,14 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/app/libs/auth-options";
 
-// export const runtime = "edge";
+const runtime = "edge";
 
 // @ts-ignore
 const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, runtime };
 
-export const GET = handler.handlers.GET;
-export const POST = handler.handlers.POST;
+// export const GET = handler.handlers.GET;
+// export const POST = handler.handlers.POST;
 
-export const runtime = "edge";
+// export const runtime = "edge";
